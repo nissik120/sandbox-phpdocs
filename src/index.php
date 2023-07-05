dfd2985a8a1ac6434ff514a371715493fe4969d7
Nissi Lawrence Kazembe <nissik120@gmail.com>
2023-07-06 00:16:24 +0200

    EDIT. writing some sample documentation

--- src/index.php
+++ src/index.php
@@ -3,9 +3,32 @@
 <title>PHP Test</title>
 </head>
 <body>
-<?php 
+<?php
+/**
+ *  Hello World Echoing and PHP info querying
+ * 
+ */
+
+
     echo '<p>Hello World</p>'; 
     phpinfo();
+
+/**
+ * A summary informing the user what the associated element does.
+ *
+ * A *description*, that can span multiple lines, to go _in-depth_ into
+ * the details of this element and to provide some background information
+ * or textual references.
+ *
+ * @param string $myArgument With a *description* of this argument,
+ *                           these may also span multiple lines.
+ *
+ * @return void
+ */
+function myFunction($myArgument)
+{
+}
+
 ?>
 </body>
 </html>
