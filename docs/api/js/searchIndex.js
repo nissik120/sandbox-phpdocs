59ba0340171068e87927adadb4bd9973cc17c9d0
Nissi Lawrence Kazembe <nissik120@gmail.com>
2023-07-06 00:44:08 +0200

    CHG. Updated phpDocumentor.phar into root location

--- docs/api/js/searchIndex.js
+++ docs/api/js/searchIndex.js
@@ -1,6 +1,11 @@
 Search.appendIndex(
     [
                 {
+            "fqsen": "\\myFunction\u0028\u0029",
+            "name": "myFunction",
+            "summary": "A\u0020summary\u0020informing\u0020the\u0020user\u0020what\u0020the\u0020associated\u0020element\u0020does.",
+            "url": "namespaces/default.html#function_myFunction"
+        },                {
             "fqsen": "\\",
             "name": "\\",
             "summary": "",
